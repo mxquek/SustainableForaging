9cf125229f8ef7ad317f40b42916560eb1f327f5
Mabel Quek <mxquek1@gmail.com>
2022-04-01 20:37:54 -0500

    Initial Commit

---
+++ SustainableForaging.BLL/ForagerService.cs
@@ -0,0 +1,29 @@
+﻿using SustainableForaging.Core.Models;
+using SustainableForaging.Core.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SustainableForaging.BLL
+{
+    public class ForagerService
+    {
+        private readonly IForagerRepository repository;
+
+        public ForagerService(IForagerRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<Forager> FindByState(string stateAbbr)
+        {
+            return repository.FindByState(stateAbbr);
+        }
+
+        public List<Forager> FindByLastName(string prefix)
+        {
+            return repository.FindAll()
+                    .Where(i => i.LastName.StartsWith(prefix))
+                    .ToList();
+        }
+    }
+}
---
+++ SustainableForaging.UI/Program.cs
@@ -0,0 +1,34 @@
+﻿using SustainableForaging.BLL;
+using SustainableForaging.DAL;
+using System;
+using System.IO;
+
+namespace SustainableForaging.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            ConsoleIO io = new ConsoleIO();
+            View view = new View(io);
+
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string forageFileDirectory = Path.Combine(projectDirectory, "data", "forage_data");
+            string foragerFilePath = Path.Combine(projectDirectory, "data", "foragers.csv");
+            string itemFilePath = Path.Combine(projectDirectory, "data", "items.txt");
+
+            ForageFileRepository forageFileRepository = new ForageFileRepository(forageFileDirectory);
+            ForagerFileRepository foragerFileRepository = new ForagerFileRepository(foragerFilePath);
+            ItemFileRepository itemFileRepository = new ItemFileRepository(itemFilePath);
+
+            ForagerService foragerService = new ForagerService(foragerFileRepository);
+            ForageService forageService = new ForageService(forageFileRepository, foragerFileRepository, itemFileRepository);
+            ItemService itemService = new ItemService(itemFileRepository);
+
+            Controller controller = new Controller(foragerService, forageService, itemService, view);
+            controller.Run();
+        }
+
+    }
+}
